7ebc08dcb73a7e94fd116d634696e314a2e371ee
Sathish <115335195+sathi2536@users.noreply.github.com>
2023-04-12 11:18:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// var container=document.createElement("div")
+// container.className="container"
+// var row=document.createElement("div")
+// row.className="row"
+// var col=document.createElement("div")
+// col.className="col"
+// col.innerHTML="this is cool"
+// document.body.append(container)
+// container.append(row)
+// row.append(col)
+
+function foo(){
+    let name=document.getElementById("name").value
+    console.log("Firstname:"+" "+name)
+    let midname=document.getElementById("midname").value
+    console.log("middle name:"+" "+midname)
+    let lasname=document.getElementById("lasname").value
+    console.log("Last name:"+" "+lasname)
+    let addres=document.getElementById("Address").value
+    console.log("addres:"+" "+addres)
+    let password=document.getElementById("password").value
+    console.log("Password:"+" "+password)
+
+
+
+
+}
